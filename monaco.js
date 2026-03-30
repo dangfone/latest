1d1c95a69e5fef863a5316c324e52e25542754e4
dangfone <90044435+dangfone@users.noreply.github.com>
2026-03-30 16:43:49 +0700

    Update monaco.js

--- monaco.js
+++ monaco.js
@@ -19,7 +19,7 @@ require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.0
 	}
  });
   
-  
+monaco.editor.EditorOptions.stickyScroll = { enabled: false };
 monaco.languages.html.htmlDefaults.setOptions({
    suggest: { html5: true }
 });

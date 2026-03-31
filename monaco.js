e1d6fcf583c3d24c30ae4e9b9eaadd5928d88076
dangfone <90044435+dangfone@users.noreply.github.com>
2026-03-31 12:15:54 +0700

    Refactor Monaco editor initialization and setup

--- monaco.js
+++ monaco.js
@@ -120,6 +120,7 @@ var ids=0
 
 	editor.saveLineInfo = async function(line){
 		console.log(line)
+		console.log(this.settingLine)
 		if(this.settingLine) return
 		await boss.dbBoss.setTabLine(this.tabID,line)
 	}

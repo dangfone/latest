83a7e592a781026be5fc36495beb62f7b8cbdfb1
dangfone <90044435+dangfone@users.noreply.github.com>
2026-04-02 17:10:26 +0700

    Refactor jsTabs.js for improved readability

--- jsTabs.js
+++ jsTabs.js
@@ -119,7 +119,7 @@ boss.setupJsTabs = function(){
 			
 			edi.setTab(tab.id)
 			var t = await boss.dbBoss.getTab(tab.id)
-			edi.load(t.data,tab.line)
+			edi.load(t.data,t.line)
 			await boss.dbBoss.setProjectLastTab(tab)
 			
 		},
